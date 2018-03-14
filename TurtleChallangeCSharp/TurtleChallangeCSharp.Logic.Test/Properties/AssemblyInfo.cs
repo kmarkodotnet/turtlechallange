3ad189a86735fa340696daa4d2741afec12262f0
K2\marko <kmarko.net@gmail.com>
2018-03-14 11:59:10 +0100

    projects

---
+++ TurtleChallangeCSharp/TurtleChallangeCSharp.Logic.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TurtleChallangeCSharp.Logic.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TurtleChallangeCSharp.Logic.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16e2478f-7070-4ac4-84e4-4a8b206bf74d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
